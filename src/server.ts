5d15053c854848906ea4c06cd27014bf2931837b
Svyatoslav Nesteruk <2626246q@gmail.com>
2020-05-04 15:11:38 +0300

    delete middleware

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
-import bodyParser = require('body-parser');
 import express = require('express');
-import session = require('express-session');
+import bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
-}));
 
 app.get('/api', (req, res) => {
   res.send('api test');
